c16ac47fb0fad8ad11fca50affd3bdfc78ccbf81
Anastasia I <niavolk@gmail.com>
2023-01-07 23:45:08 +0400

    Reformated Code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         BmiService service = new BmiService();
         int weight = 68; //вес в килограммах
         int height = 165; //рост в сантиметрах
-
         double bmi = service.calculate(weight, height);
         String formattedBmi = String.format("%.2f", bmi); //форматирование индекса bmi до двух знаков после запятой
         System.out.println("Ваш индекс = " + formattedBmi);
